ff33bf775a76b2bed1ce62473d2c4c55b1187b19
Puneeth G S <108973891+Puneeth2010@users.noreply.github.com>
2022-07-13 10:06:19 +0530

    Create DAY1_PROB_5.c

---
+++ DAY1/DAY1_PROB_5.c
@@ -0,0 +1,49 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   char b1,b2,b3,b4,b5;
+   printf("Read the 5 Balls number from user:\n");
+   scanf(" %c %c %c %c %c",&b1,&b2,&b3,&b4,&b5);
+   if(b1 % 2 == 0)
+   {
+       printf("the b1 ball of numbered %c is even number\n", b1);
+   }
+   else
+   {
+       printf("the b1 ball of numbered %c is odd number\n", b1);
+   }
+   if(b2 % 2 == 0)
+   {
+       printf("the b2 ball of numbered %c is even number\n", b2);
+   }
+   else
+   {
+       printf("the b2 ball of numbered %c is odd number\n", b2);
+   }
+   if(b3 % 2 == 0)
+   {
+       printf("the b3 ball of numbered %c is even number\n", b3);
+   }
+   else
+   {
+       printf("the b3 ball of numbered %c is odd number\n", b3);
+   }
+   if(b4 % 2 == 0)
+   {
+       printf("the b4 ball of numbered %c is even number\n", b4);
+   }
+   else
+   {
+       printf("the b4 ball of numbered %c is odd number\n", b4);
+   }if(b5 % 2 == 0)
+   {
+       printf("the b5 ball of numbered %c is even number\n", b5);
+   }
+   else
+   {
+       printf("the b5 ball of numbered %c is odd number\n", b5);
+   }
+  
+     return 0;
+}
