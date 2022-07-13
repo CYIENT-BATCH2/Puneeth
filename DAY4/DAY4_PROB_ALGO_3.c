b25dd68ad372130d0d7bdc529a0d4ee1a5ac661b
Puneeth G S <108973891+Puneeth2010@users.noreply.github.com>
2022-07-13 15:39:20 +0530

    Create DAY4_PROB_ALGO_3.c

---
+++ DAY4/DAY4_PROB_ALGO_3.c
@@ -0,0 +1,16 @@
+/*FINDOUT THE VALUE FOR i,j,k USING THE LOGICAL OPERATION
+
+#include<stdio.h>
+int main()
+{
+    int i=10,j=20,k;
+    
+    k=i&&j;  
+    printf("k value of k=i&&j is %d\n", k);
+    k=i||j;
+    printf("k value of k=i||j is %d\n", k);
+    k=!i;
+    printf("k value of k=!i is %d\n", k);
+    return 0;
+}
+*/
